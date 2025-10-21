920f1d9fb0ddf845f2cc8a2bb405b806602e16eb
Lari Taskula <lari.taskula@hypernova.fi>
2025-10-21 22:34:18 +0300

    Bug 35423: Fix substr outside of string warning
    To test:
    
    Before applying patch
    
    1. Create an authority record, fill field 400$w with "n"
    2. Search for the authority you just created
    3. Observe a warning has been logged
    
    "substr outside of string"
    
    4. Apply patch
    5. Refresh the search results from step 2
    6. Observe no warnings have been logged

--- lib/C4/AuthoritiesMarc.pm
+++ lib/C4/AuthoritiesMarc.pm
@@ -1084,7 +1084,7 @@ sub BuildSummary {
             my $type = 'seefrom';
             $type = ( $marc21controlrefs{ substr $field->subfield('w'), 0, 1 } || '' ) if ( $field->subfield('w') );
             if ( $type eq 'notapplicable' ) {
-                $type = substr $field->subfield('w'), 2, 1;
+                $type = substr $field->subfield('w'), 2, 1 if length( $field->subfield('w') ) > 2;
                 $type = 'earlier' if $type && $type ne 'n';
             }
             if ( $type eq 'subfi' ) {
@@ -1107,7 +1107,7 @@ sub BuildSummary {
             my $type = 'seealso';
             $type = ( $marc21controlrefs{ substr $field->subfield('w'), 0, 1 } || '' ) if ( $field->subfield('w') );
             if ( $type eq 'notapplicable' ) {
-                $type = substr $field->subfield('w'), 2, 1;
+                $type = substr $field->subfield('w'), 2, 1 if length( $field->subfield('w') ) > 2;
                 $type = 'earlier' if $type && $type ne 'n';
             }
             if ( $type eq 'subfi' ) {
